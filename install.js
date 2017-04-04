eee4d19f460145d806600d4ef999d493ae805b20
Liby99 <liby99@icloud.com>
2017-04-04 00:41:04 -0700

    Fixed installation bug

--- install.js
+++ install.js
@@ -1,7 +1,7 @@
 var fs = require("fs");
 
-fs.mkdirSync("/public");
-fs.mkdirSync("/data");
-fs.mkdirSync("/router");
-fs.mkdirSync("/ajax");
-fs.mkdirSync("/schedule");
+fs.mkdirSync("public");
+fs.mkdirSync("data");
+fs.mkdirSync("router");
+fs.mkdirSync("ajax");
+fs.mkdirSync("schedule");
