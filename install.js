241b2149a146b2026fa99ee6b35bdad282a88d56
Liby99 <liby99@icloud.com>
2017-04-04 00:53:07 -0700

    Modified install script

--- install.js
+++ install.js
@@ -6,4 +6,4 @@ fs.mkdirSync("../../router");
 fs.mkdirSync("../../ajax");
 fs.mkdirSync("../../schedule");
 
-fs.writeFileSync("../../data/config.json", JSON.stringify(require("./lib/data/config.json")));
+fs.createReadStream("./lib/data/config.json").pipe(fs.createWriteStream("../../data/config.json"));
