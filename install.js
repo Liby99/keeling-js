8eb943fee9f0ced2cad66c430c1c7addd28719e9
Liby99 <liby99@icloud.com>
2017-04-05 23:43:44 -0700

    Updating config path customization

--- install.js
+++ install.js
@@ -1,21 +1,27 @@
 var fs = require("fs");
 var config = require("./lib/data/config.json");
+var dataPath = "../../data/";
 
-fs.mkdirSync("../../public");
-fs.mkdirSync("../../data");
-fs.mkdirSync("../../" + config["router_directory"]);
-fs.mkdirSync("../../" + config["ajax_directory"]);
-fs.mkdirSync("../../" + config["schedule_directory"]);
-
-if (!fs.existsSync("../../data/config.json")) {
-    fs.createReadStream("./lib/data/config.json").pipe(fs.createWriteStream("../../data/config.json"));
+if (!fs.existsSync(dataPath + "config.json")) {
+    fs.createReadStream("./lib/data/config.json").pipe(fs.createWriteStream(dataPath + "config.json"));
 }
 else {
-    var userConfig = require("../../data/config.json");
+    var userConfig = require(dataPath + "config.json");
     for (var i in config) {
         if (!userConfig[i]) {
             userConfig[i] = config[i];
         }
     }
-    fs.writeFileSync("../../data/config.json", JSON.stringify(userConfig));
+    fs.writeFileSync(dataPath + "config.json", JSON.stringify(userConfig));
+    config = userConfig;
 }
+
+var publicPath = "../../" + config["public_directory"];
+var routerPath = "../../" + config["router_directory"];
+var ajaxPath = "../../" + config["ajax_directory"];
+var schedulePath = "../../" + config["schedule_directory"];
+
+if (!fs.existsSync(publicPath)) fs.mkdirSync(publicPath);
+if (!fs.existsSync(routerPath)) fs.mkdirSync(routerPath);
+if (!fs.existsSync(ajaxPath)) fs.mkdirSync(ajaxPath);
+if (!fs.existsSync(schedulePath)) fs.mkdirSync(schedulePath);
