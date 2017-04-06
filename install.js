818162fc12193d2bb0061bacf0f8f06c1219013c
Liby99 <liby99@icloud.com>
2017-04-05 22:57:31 -0700

    Add Customizable Path Specification

--- install.js
+++ install.js
@@ -1,9 +1,21 @@
 var fs = require("fs");
+var config = require("./lib/data/config.json");
 
 fs.mkdirSync("../../public");
 fs.mkdirSync("../../data");
-fs.mkdirSync("../../router");
-fs.mkdirSync("../../ajax");
-fs.mkdirSync("../../schedule");
+fs.mkdirSync("../../" + config["router_directory"]);
+fs.mkdirSync("../../" + config["ajax_directory"]);
+fs.mkdirSync("../../" + config["schedule_directory"]);
 
-fs.createReadStream("./lib/data/config.json").pipe(fs.createWriteStream("../../data/config.json"));
+if (!fs.existsSync("../../data/config.json")) {
+    fs.createReadStream("./lib/data/config.json").pipe(fs.createWriteStream("../../data/config.json"));
+}
+else {
+    var userConfig = require("../../data/config.json");
+    for (var i in config) {
+        if (!userConfig[i]) {
+            userConfig[i] = config[i];
+        }
+    }
+    fs.writeFileSync("../../data/config.json", JSON.stringify(userConfig));
+}
